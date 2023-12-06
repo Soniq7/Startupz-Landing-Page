32e20bad97a0d2ad0b0d609efd668495ebc95fb0
Soniq7 <ssoniq7@gmail.com>
2023-12-06 23:44:37 +0100

    theme.js color collection created

--- src/common/theme.js
+++ src/common/theme.js
@@ -2,6 +2,14 @@ const colorCollection = {
     oxfordBlue: "#3D4F5C",
     coral: "#FB8958",
     white: "#FFFFFF",
+    pelorous: "#46B8C8",
+    swampGreen: "#A9BC87",
+    glacier: "#8BB5C9",
+    bondiBlue: "#00A0B6",
+    trueV: "#8B60D3",
+    hintOfRed: "#FBFAFA",
+    indianKhaki: "#BBA98E",
+    texasRose: "#FFC351",
 };
 
 export const theme = {
@@ -10,6 +18,26 @@ export const theme = {
             primaryText: colorCollection.oxfordBlue,
             primaryOrange: colorCollection.coral,
             textWhite: colorCollection.white,
+            textTeal: colorCollection.pelorous,
+            textGreen: colorCollection.swampGreen,
+            textLightBlue: colorCollection.glacier,
+            textBlue: colorCollection.bondiBlue,
+            textPurple: colorCollection.trueV,
+
         },
+        border: {
+            borderBlue: colorCollection.pelorous,
+            borderOrange: colorCollection.coral,
+        },
+        shadow: {
+            shadowCream: colorCollection.indianKhaki,
+        },
+        background: {
+            backgroundGrey: colorCollection,oxfordBlue,
+            backgroundWhite: colorCollection.white,
+            backgroundCream: colorCollection.hintOfRed,
+            backgroundTeal: colorCollection.pelorous,
+            backgroundYellow: colorCollection.texasRose,
+        }
     },
 };
